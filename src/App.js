85d3ae401a3c504d1bde3cd71c0e632671c0ba0b
David Pal <paldavid2030@gmail.com>
2019-11-12 13:20:37 +0300

    add two way data binding

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 import Person from './Person/Person';
 
 class App extends Component {
+
   state = {
     persons: [
       { name: "david", age: 34 },
@@ -12,28 +13,46 @@ class App extends Component {
     ]
   }
 
-  changeNameHandler = () => {
+  changeNameHandler = (name) => {
     this.setState({
       persons: [
         ...this.state.persons,
-        { name: 'Tatuka', age: 87 }
+        { name: name, age: 87 }
       ],
       wasClicked: true
-    }, () => console.log(this.state));
+    });
   };
+
+  handleInput = (event) => {
+    this.setState({
+      persons: [
+        { name: "david", age: 34 },
+        { name: event.target.value, age: 36 },
+        { name: "petra", age: 33 }
+      ]
+    })
+  }
 
   render() {
     return (
       <div className='App'>
         <h1>Hi, from the App component</h1>
-        <button onClick={this.changeNameHandler}>Switch name</button>
-        <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
-        <Person name={this.state.persons[1].name} age={this.state.persons[1].age} />
-        <Person name={this.state.persons[2].name} age={this.state.persons[2].age} />
+        <button onClick={this.changeNameHandler.bind(this, 'Davidkaaaa')}>Switch name</button>
+        <Person name={this.state.persons[0].name} age={this.state.persons[0].age} click={this.changeNameHandler} />
+        <Person name={this.state.persons[1].name} age={this.state.persons[1].age} click={this.changeNameHandler} changed={this.handleInput}/>
+        <Person name={this.state.persons[2].name} age={this.state.persons[2].age} click={this.changeNameHandler} />
       </div>
     );
-    // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'does it work now?'));
   }
+  // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'does it work now?'));
 }
 
 export default App;
+
+// state = {
+//   persons: [
+//     { name: "david", age: 34 },
+//     { name: "ana", age: 36 },
+//     { name: "petra", age: 33 }
+//   ]
+// }
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -5,7 +5,8 @@ import './person.css';
 const person = (props) => {
   return (
     <div>
-      <h2 className="mono">Im {props.name}, I am {props.age} years old.</h2>
+      <h2 onClick={props.click} className="mono">Im {props.name}, I am {props.age} years old.</h2>
+      <input type="text" onChange={props.changed}/>
     </div>
   )
   
