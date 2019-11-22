0da74fbd2a5a9a893031957fbd98e935d1503dad
David Pal <paldavid2030@gmail.com>
2019-11-22 16:36:42 +0300

    re-introducing styled-components and fix focus issue

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,6 +1,27 @@
 import React from 'react';
-import './person.css';
+import styled from 'styled-components';
+// import './person.css';
 
+const StyledDiv = styled.div`
+    width: 100%;
+    border: 2px solid black;
+    box-shadow: 0 2px 20px black;
+    padding: 15px;
+    box-sizing: border-box;
+  
+  :not(:last-child) {
+    margin-bottom: 20px;
+  }
+  
+  .mono {
+    font-family: monospace;
+    color: red;
+  }  
+
+  @media (min-width: 500px) {
+    width: 450px;
+  }
+  `;
 
 const Person = (props) => {
 
@@ -8,11 +29,12 @@ const Person = (props) => {
     borderRadius: '20px'
   }
 
+  
   return (
-    <div className="Person">
+    <StyledDiv>
       <h2 className="mono" onClick={props.click}>Im {props.name}, I am {props.age} years old.</h2>
       <input type="text" onChange={props.changed} placeholder={props.name} style={inputStyle} />
-    </div>
+    </StyledDiv >
   )
 
 }
