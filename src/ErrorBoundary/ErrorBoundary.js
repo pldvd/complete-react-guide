1325693c25782ecd1502c9dcafe8d9a73afcab9b
David Pal <paldavid2030@gmail.com>
2019-11-27 10:17:19 +0300

    start building out ErrorBoundary

---
+++ src/ErrorBoundary/ErrorBoundary.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class ErrorBoundary extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      hasError: false
+    }
+  }
+
+  //to FINISH!!!
+}
